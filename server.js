e972d73d3291bc5ba97186b977f1c329dbcbaf52
Hunter <88465412+Hunyeh@users.noreply.github.com>
2022-01-05 20:53:16 -0500

    errors

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ mongoose.connect(MONGODB_URI,  {
 // routes
 app.use(require("./routes/api.js"));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
